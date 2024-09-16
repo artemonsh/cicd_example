56d95a5f484a2e1a53f72b6ca4c6f39df7ad3052
Артём Шумейко <younginterceptor@gmail.com>
2024-09-16 22:59:38 +0300

    fix test world

--- test_api.py
+++ test_api.py
@@ -9,4 +9,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello Wrld!"}
+    assert response.json() == {"msg": "Hello World!"}
