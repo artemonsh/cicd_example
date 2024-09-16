d54575b0121859d18b4b0158b0c97c5f1f147801
Artem Shumeyko <shuminattor@gmail.com>
2024-09-16 21:29:45 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.12
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install -r requirements.txt
+
+COPY . .
---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"msg": "Hello Wrld!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", port=8000)
